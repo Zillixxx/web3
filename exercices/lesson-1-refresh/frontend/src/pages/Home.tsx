8bbb3eb723f591e1ec44ba32e89c97f69eadb1ee
Zillixxx <arthur.verhoest@student.vinci.be>
2025-09-26 08:50:02 +0200

    Travail local avant merge

--- exercices/lesson-1-refresh/frontend/src/pages/Home.tsx
+++ exercices/lesson-1-refresh/frontend/src/pages/Home.tsx
@@ -4,29 +4,7 @@ import ExpenseAdd from "../components/ExpenseAdd";
 import type { Expense } from "../types/Expenses";
 
 const Home: React.FC = () => {
-  const [expenses, setExpenses] = useState<Expense[]>([
-    {
-      id: "1",
-      date: "2024-06-01",
-      description: "Groceries",
-      payer: "Alice",
-      amount: 150.75,
-    },
-    {
-      id: "2",
-      date: "2024-06-03",
-      description: "Utilities",
-      payer: "Bob",
-      amount: 80.5,
-    },
-    {
-      id: "3",
-      date: "2024-06-05",
-      description: "Dining Out",
-      payer: "Charlie",
-      amount: 60.0,
-    },
-  ]);
+  const [expenses, setExpenses] = useState<Expense[]>([]);
 
   const handleAddExpense = (newExpense: Expense) => {
     setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
